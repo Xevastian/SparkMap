676d5ef7db64f067c3d10709b4b14a1a071ed1ee
cicadatreatment <22-06230@g.batstate-u.edu.ph>
2023-05-09 22:19:23 +0800

    Revised arduino_data

--- Database/arduino_data.py
+++ Database/arduino_data.py
@@ -16,13 +16,23 @@ db = mysql.connector.connect(
 
 # Loop to read data from serial port and insert into database
 while True:
-    line = ser.readline().decode().strip()  # Read a line of data from the serial port
-    Electric_consumption = line
-    now = datetime.now()
-    cursor = db.cursor()
-    query = "INSERT into data (date, time, Electric_consumption) VALUES (%s, %s, %s)"
-    values = (now.date(), now.time(), Electric_consumption)
-    cursor.execute(query, values)
+    # Read data from serial port
+    data = ser.readline().decode('utf-8').rstrip()  # Read a line of data from the serial port
+    
+    # Split data into separate values
+    values = data.split(',')
+
+    # Check if there are 5 values (overall, ACES, CICS, CEAFAD, CIT)
+    if len(values) == 5:
+        Overall, ACES, CICS, CEAFAD, CIT = values
+
+        # Insert data into MySQL database
+        cursor = db.cursor()
+        now = datetime.now()
+        query = "INSERT into data (date, time, Overall, ACES, CICS, CEAFAD, CIT) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        val = (now.date(), now.time(), Overall, ACES, CICS, CEAFAD, CIT)
+        cursor.execute(query, val)
+
 
     # Fetch all rows
     query = "SELECT * FROM data"
@@ -31,10 +41,13 @@ while True:
     for row in result:
         date_str = row[0].strftime('%Y-%m-%d')
         time_str = (datetime.min + row[1]).time().strftime('%H:%M:%S')
-        electric_consumption = row[2]
-        status = row[3]
+        overall = row[2]
+        aces = row[3]
+        cics = row[4]
+        ceafad = row[5]
+        cit = row[6]
 
-        print(f"Date: {date_str}, Time: {time_str}, Electric Consumption: {electric_consumption}, Status: {status}")
+        print(f"Date: {date_str}, Time: {time_str}, Overall: {overall}, ACES: {aces}, CICS: {cics}, CEAFAD: {ceafad}, CIT: {cit}")
 
     db.commit()
     cursor.close()
